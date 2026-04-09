a67ce342ec4dedc608e138a6f75f43c41f93a572
paulahaas <phaas2007@gmail.com>
2026-04-09 20:28:35 -0300

    cria endpoint /integrantes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', { root: __dirname })
+})
+
+const data = {
+  integrantes: [
+    { nome: 'Marcella Cagliari'},    { nome: 'Paula Haas'},  ]
+}
+
+app.get('/integrantes', (req, res) => {
+  res.json(data)
+})
+
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000')
 })
